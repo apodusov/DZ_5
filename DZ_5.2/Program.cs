9c82b0044c0e038b3126a7af25d683abef94919a
Aleksey Podusov <apodusov@gmail.com>
2022-09-19 12:52:55 +0300

    +1 Задача 5.36

---
+++ DZ_5.2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива");
+int number = int.Parse(Console.ReadLine());
+
+
+int[] array = new int[number];
+
+void FindsumArray(int[] array)
+
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+
+    int sum = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+        sum = array[i] + sum;
+    }
+
+    Console.WriteLine(sum);
+}
+FindsumArray(array);
