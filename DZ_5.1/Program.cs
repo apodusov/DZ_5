6ed2c7c7f8a56bad0ba4d8d16dac8d69e27e7650
Aleksey Podusov <apodusov@gmail.com>
2022-09-19 12:49:20 +0300

    +1 Задача 5.34

---
+++ DZ_5.1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+//которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива");
+int number = int.Parse(Console.ReadLine());
+
+int[] array = new int[number];
+
+void FindevenArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(99, 1000);
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Количество четных чисел = {count}");
+}
+FindevenArray(array);
